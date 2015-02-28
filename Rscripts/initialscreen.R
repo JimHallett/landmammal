23d8f6ed5a72ee8a45db863307bbbaa94a486773
nmagone <nmagone@gmail.com>
2015-02-27 16:17:28 -0800

    initalpipeline

--- Rscripts/initialscreen.R
+++ Rscripts/initialscreen.R
@@ -1,6 +1,7 @@
 library(RODBC)
 library(tidyr)
 library(dplyr)
+library(lubridate)
 
 # Connect to external database
 
@@ -19,7 +20,7 @@ sm$Year <- year(sm$Date)
 
 smSubset <- subset(sm, select = c("Site", "LandNumber", "Year", "Station", "Species", "Sex", "Weight", "TotalBody"))
 
-
+smSubset$TotalBody <- as.numeric(smSubset$TotalBody)
 
 table(smSubset$Species)
 Species.freq <- table(smSubset$Species)
@@ -44,17 +45,27 @@ dim(Species.Dates)
 Species.obj <- table(smSubset$Species, smSubset$Year)
 
 
+#########################################################################################
+# Means Weight and TotalBody
+#########################################################################################
+
+smSubsetMeans <- smSubset %>%
+  filter(Year==1993) %>%                                 
+  arrange(Species, Sex) %>%
+  group_by(Species, Sex) %>%
+  summarise(meanWeight = mean(Weight))
+  
+
 arrange(smSubset, Year, Species, Sex, Weight, TotalBody)
 
 smSubset[ order(smSubset$Year, smSubset$Species, smSubset$Sex, smSubset$Weight, smSubset$TotalBody), ]
 
+options(digits=1)
+
+#filter(smSubset, Species=="SOVA", Site=="MFC REG 4", Year==1993,Sex=="M")
 
 
-filter(smSubset, Species==SOVA, Site==MFC REG 4, Year==1993,sex==M)
-
-
-
-
+firstmeantest <- group_by(smSubset, Species, Site, Year, Sex, mean(Weight, TotalBody))
 
 
 
